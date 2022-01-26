2e21339efe7070a6d39baa76a52b182444fc95ef
brice-morgat <brice.morgat@epitech.eu>
2022-01-26 13:01:30 +0100

    Init Spring Project

---
+++ src/test/java/fr/safetynet/alerts/SafetyNetAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.safetynet.alerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyNetAlertsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
